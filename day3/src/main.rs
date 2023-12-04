d8be07892b277852f3d8d2092ee2a1a25afc1ac6
Zachary Taylor <zacktaylor97@gmail.com>
2023-12-04 20:32:42 +1000

    refactoring

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,5 +1,4 @@
-use core::num;
-use std::{cmp, fs};
+use std::fs;
 
 fn main() {
     let document = read_file("./input.txt");
@@ -11,7 +10,6 @@ fn main() {
 
 fn part_one<'a>(lines: &'a Vec<&'a str>) {
     let symbols: Vec<char> = vec!['/', '@', '*', '%', '#', '=', '+', '&', '-', '$'];
-
     let valid_cells = get_valid_cells(lines, &symbols);
 
     let mut sum: u64 = 0;
@@ -25,14 +23,8 @@ fn part_one<'a>(lines: &'a Vec<&'a str>) {
         for num_pair in 0..grid_nums[row].len() {
             for col in 0..grid_nums[row][num_pair].1.len() {
                 if valid_cells[row][grid_nums[row][num_pair].1[col]] {
-
                     if grid_nums[row][num_pair].0 != 0 {
-                        println!(
-                            "row {row} col {} num {}",
-                            grid_nums[row][num_pair].1[col], grid_nums[row][num_pair].0
-                        );
-
-                        sum += grid_nums[row][num_pair].0 as u64;
+                        sum += u64::from(grid_nums[row][num_pair].0);
                         grid_nums[row][num_pair].0 = 0;
                     }
                 }
@@ -40,12 +32,10 @@ fn part_one<'a>(lines: &'a Vec<&'a str>) {
         }
     }
 
-    println!("{sum}");
+    println!("Part one ans: {sum}");
 }
 
 fn part_two(lines: &Vec<&str>) {
-    let symbols: Vec<char> = vec!['*'];
-
     let valid_cells = get_symbol_cells(lines, '*');
 
     let grid_nums = lines
@@ -53,20 +43,19 @@ fn part_two(lines: &Vec<&str>) {
         .map(|line| get_nums_from_row(line))
         .collect::<Vec<_>>();
 
-
-        let mut sum: u64 = 0;
+    let mut sum: u64 = 0;
 
     // Each *
     for (row, col) in valid_cells {
-
         let mut num_buf: Vec<u64> = vec![];
         // Check each row
         for grid_row in [&grid_nums[row - 1], &grid_nums[row], &grid_nums[row + 1]] {
             // Check each number in each row
             for (num, indices) in grid_row {
+
                 for idx in indices {
                     if *idx == col || *idx == col - 1 || *idx == col + 1 {
-                        num_buf.push(*num as u64);
+                        num_buf.push(u64::from(*num));
                         break;
                     }
                 }
@@ -74,39 +63,31 @@ fn part_two(lines: &Vec<&str>) {
         }
 
         if num_buf.len() == 2 {
-            println!("row {row} col {col} nums = {} {}", num_buf[0], num_buf[1]);
-            
-            sum += num_buf[0] as u64 * num_buf[1] as u64;
+            sum += u64::from(num_buf[0]) * u64::from(num_buf[1]);
         }
 
         num_buf.clear();
     }
 
-    println!("{sum}");
+    println!("Part two ans: {sum}");
 }
 
+/// Get indices of all cells occupied by the input symbol
 fn get_symbol_cells(lines: &Vec<&str>, symbol: char) -> Vec<(usize, usize)> {
-    let mut output: Vec<(usize, usize)> = vec![];
-
-    for (row, line) in lines.iter().enumerate() {
-        let num_cols = line.len();
-
-        let row = row;
-
-        for (col, _) in line.match_indices(symbol) {
-            let col = col;
-
-            output.push((row, col));
-        }
-    }
-
-    output
+    lines
+        .iter()
+        .enumerate()
+        .flat_map(|(row, line)| {
+            line.bytes()
+                .enumerate()
+                .filter(|(_, ch)| char::from(*ch) == symbol)
+                .map(move |(col, _)| (row, col))
+        })
+        .collect::<Vec<_>>()
 }
 
 fn get_valid_cells(lines: &Vec<&str>, symbols: &Vec<char>) -> [[bool; 140]; 140] {
     let num_rows = lines.len() as i32;
-
-    // get valid cells to check for numbers
     let mut valid_cells = [[false; 140]; 140];
 
     for (row, line) in lines.iter().enumerate() {
@@ -117,7 +98,6 @@ fn get_valid_cells(lines: &Vec<&str>, symbols: &Vec<char>) -> [[bool; 140]; 140]
         for sym in symbols {
             for (col, _) in line.match_indices(*sym) {
                 let col = col as i32;
-
                 for i in -1..=1 {
                     for j in -1..=1 {
                         if row + i >= 0
@@ -126,7 +106,6 @@ fn get_valid_cells(lines: &Vec<&str>, symbols: &Vec<char>) -> [[bool; 140]; 140]
                             && col + j <= num_cols
                         {
                             valid_cells[(row + i) as usize][(col + j) as usize] = true;
-                            // valid_cells.push(((row + i) as usize, (col + j) as usize));
                         }
                     }
                 }
@@ -139,32 +118,15 @@ fn get_valid_cells(lines: &Vec<&str>, symbols: &Vec<char>) -> [[bool; 140]; 140]
 fn get_nums_from_row(line: &str) -> Vec<(u32, Vec<usize>)> {
     let mut num_vec: Vec<(u32, Vec<usize>)> = vec![];
     let mut num_buf: Vec<(u8, usize)> = vec![];
+
     for (idx, ch) in line.bytes().enumerate() {
-        if ch.is_ascii_digit() && idx < 139 {
+        if ch.is_ascii_digit() {
             num_buf.push((ch, idx));
-        } else if ch.is_ascii_digit() && idx == 139 {
-            num_buf.push((ch, idx));
-            if num_buf.len() > 0 {
-                let num = num_buf
-                    .iter()
-                    .map(|el| el.0 as char)
-                    .collect::<String>()
-                    .parse::<u32>()
-                    .expect("error parsing number");
-                let indices = num_buf.iter().map(|el| el.1).collect::<Vec<usize>>();
+        }
 
-                num_vec.push((num, indices));
-            }
-
-            num_buf.clear();
-        } else {
+        if !ch.is_ascii_digit() || idx == 139 {
             if num_buf.len() > 0 {
-                let num = num_buf
-                    .iter()
-                    .map(|el| el.0 as char)
-                    .collect::<String>()
-                    .parse::<u32>()
-                    .expect("error parsing number");
+                let num = buf_to_num(&num_buf);
                 let indices = num_buf.iter().map(|el| el.1).collect::<Vec<usize>>();
 
                 num_vec.push((num, indices));
@@ -173,9 +135,21 @@ fn get_nums_from_row(line: &str) -> Vec<(u32, Vec<usize>)> {
             num_buf.clear();
         }
     }
+
     num_vec
 }
 
+fn buf_to_num(num_buf: &Vec<(u8, usize)>) -> u32 {
+    let num = num_buf
+        .iter()
+        .map(|el| char::from(el.0))
+        .collect::<String>()
+        .parse::<u32>()
+        .expect("error parsing number");
+    num
+}
+
+/// Debug function to check grid of cells to check
 fn print_valid_cells(valid_cells: [[bool; 140]; 140]) {
     for i in 0..valid_cells.len() {
         for j in 0..valid_cells[i].len() {
